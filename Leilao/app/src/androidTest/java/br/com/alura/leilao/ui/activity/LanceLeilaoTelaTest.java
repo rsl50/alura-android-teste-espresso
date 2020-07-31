f901a5c7588a8e907934d48ab7fd59c6b9e41eaa
Robson Lima <rsl.robson@gmail.com>
2020-07-31 15:52:57 -0300

    Implementando o fluxo até preencher o valor do lance

--- Leilao/app/src/androidTest/java/br/com/alura/leilao/ui/activity/LanceLeilaoTelaTest.java
+++ Leilao/app/src/androidTest/java/br/com/alura/leilao/ui/activity/LanceLeilaoTelaTest.java
@@ -1,6 +1,7 @@
 package br.com.alura.leilao.ui.activity;
 
 import android.content.Intent;
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.After;
@@ -16,11 +17,15 @@ import br.com.alura.leilao.model.Leilao;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 public class LanceLeilaoTelaTest extends BaseTesteIntegracao {
 
@@ -41,11 +46,11 @@ public class LanceLeilaoTelaTest extends BaseTesteIntegracao {
         // Inicializar a main activity
         mainActivity.launchActivity(new Intent());
 
-        // Clicar no leilão
+        // Clica no leilão
         onView(withId(R.id.lista_leilao_recyclerview))
                 .perform(actionOnItemAtPosition(0, click()));
 
-        // Clicar no fab da tela de lances do leilão
+        // Clica no fab da tela de lances do leilão
         onView(withId(R.id.lances_leilao_fab_adiciona))
                 .perform(click());
 
@@ -59,6 +64,51 @@ public class LanceLeilaoTelaTest extends BaseTesteIntegracao {
         // Clica no botão "Cadastrar usuário"
         onView(withText("Cadastrar usuário"))
                 .perform(click());
+
+        // Clica no fab tela de lista de usuários
+        onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        // Clica no EditText e preenche com o nome do usuário
+        onView(allOf(withId(R.id.form_usuario_nome),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.form_usuario_nome_edittext),
+                isDisplayed()))
+                .perform(replaceText("Robson"), closeSoftKeyboard());
+
+        // Clica em Adicionar
+        onView(allOf(withId(android.R.id.button1),
+                withText("Adicionar"),
+                isDisplayed()))
+                .perform(scrollTo(), click());
+
+        // Clica no botão back do Android
+        Espresso.pressBack();
+
+        // Clica no fab lances do leilão
+        onView(withId(R.id.lances_leilao_fab_adiciona))
+                .perform(click());
+
+        // Verifica visibilidade do dialog com o título esperado
+        onView(withText("Novo lance"))
+                .check(matches(isDisplayed()));
+
+        // Clica no EditText de valor e preenche
+        onView(withId(R.id.form_lance_valor_edittext))
+                .perform(click(),
+                        replaceText("200"),
+                        closeSoftKeyboard());
+
+        // Seleciona o usuário
+        onView(withId(R.id.form_lance_usuario)).perform(click());
+
+        // Clica no botão "Propor"
+
+        // Fazer assertion para as views de maior e menor lance, e também, para os tres lances
+
     }
 
     @After
